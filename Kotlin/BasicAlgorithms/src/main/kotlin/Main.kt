db2e7390383428c57d9f0ae084fdcdc7e23222bf
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-04 19:55:44 +0800

    Kotlin calculator practice, get occurrences Java version, and gitingore to handle excess node modules

--- Kotlin/BasicAlgorithms/src/main/kotlin/Main.kt
+++ Kotlin/BasicAlgorithms/src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import calculator.Calculator
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("----------------------------------------------------")
+    println("Initializing calculator class with values 10 and 2")
+    val calculator = Calculator(num1=10, num2=2)
+    println("----------------------------------------------------")
+    println("The addition value is ${calculator.add()}")
+    println("----------------------------------------------------")
+    println("The subtraction value is ${calculator.subtract()}")
+    println("----------------------------------------------------")
+    println("The division value is ${calculator.divide()}")
+    println("----------------------------------------------------" )
+    println("The multiplication value is ${calculator.multiply()}")
+    println("----------------------------------------------------" )
 }
--- Kotlin/BasicAlgorithms/src/main/kotlin/calculator/Calculator.kt
+++ Kotlin/BasicAlgorithms/src/main/kotlin/calculator/Calculator.kt
@@ -1,5 +1,24 @@
 package calculator
 
-class Calculator {
+class Calculator(
+    val num1: Int,
+    val num2: Int,
+    ) {
+
+    fun add():Int {
+        return this.num1 + this.num2;
+    }
+
+    fun subtract():Int {
+        return this.num1 - this.num2;
+    }
+
+    fun divide():Int {
+        return this.num1 / this.num2;
+    }
+
+    fun multiply():Int {
+        return this.num1 * this.num2;
+    }
     
 }
