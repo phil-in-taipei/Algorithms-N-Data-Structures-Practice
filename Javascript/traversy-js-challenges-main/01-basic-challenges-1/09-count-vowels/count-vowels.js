735a31a84decb2c6fb29dda56c28796e95bc6d89
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-06 15:16:32 +0800

    Traversy Javascript count vowels and remove duplicates

--- Javascript/traversy-js-challenges-main/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ Javascript/traversy-js-challenges-main/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(stringToCheck) {
+    const listOfVowels = ['a', 'e', 'i', 'o', 'u'];
+    let numberOfVowelsInString = 0;
+    for (let i = 0; i < stringToCheck.length; i++) {
+        if (listOfVowels.includes(stringToCheck.charAt(i).toLowerCase())) {
+            numberOfVowelsInString++;
+        }
+    }
+    return numberOfVowelsInString;
+}
 
 module.exports = countVowels;
--- Javascript/traversy-js-challenges-main/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ Javascript/traversy-js-challenges-main/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    let nonDuplicates = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!nonDuplicates.includes(arr[i])) {
+            nonDuplicates.push(arr[i]);
+        }
+    }
+    return nonDuplicates;
+}
 
 module.exports = removeDuplicates;
