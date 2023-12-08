a3f100d15b8551f305e1148eb562cd062cae7747
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-08 16:43:06 +0800

    Javascript fizzbuzz, array intersection, find missing number, and array intersection practice

--- Javascript/traversy-js-challenges-main/02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ Javascript/traversy-js-challenges-main/02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,26 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(numberToCountUntil) {
+    fizzBuzzArray = []
+    let i = 0;
+    while(i < numberToCountUntil) {
+        i++;
+        divisibleBy3Or5 = false;
+        let returnString = "";
+        if (i % 3 === 0) {
+            returnString += "Fizz"
+            divisibleBy3Or5 = true
+        }
+        if (i % 5 === 0) {
+            returnString += "Buzz"
+            divisibleBy3Or5 = true;
+        }
+        if (divisibleBy3Or5) {
+            fizzBuzzArray.push(returnString);
+        } else {
+            fizzBuzzArray.push(i);
+        }
+
+    }
+    return fizzBuzzArray;
+}
 
 module.exports = fizzBuzzArray;
--- Javascript/traversy-js-challenges-main/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ Javascript/traversy-js-challenges-main/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,15 @@
-function findMissingNumber() {}
+function findMissingNumber(arrayOfNumbers) {
+    if (arrayOfNumbers.length === 0) {
+        return 1;
+    }
+    const n = arrayOfNumbers.length + 1;
+    const expectedSumOfNumbers = (n * (n + 1) / 2);
+    let actualSum = 0;
+    for (let i=0; i < arrayOfNumbers.length; i++) {
+        actualSum += arrayOfNumbers[i]
+    }
+    return expectedSumOfNumbers - actualSum;
+
+}
 
 module.exports = findMissingNumber;
