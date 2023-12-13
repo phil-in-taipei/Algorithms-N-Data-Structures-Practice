a5cfb2f44e9a086ebd50f2fb2365995e164e3508
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-13 14:34:58 +0800

    Find missing letter javascript implementation

--- Javascript/traversy-js-challenges-main/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ Javascript/traversy-js-challenges-main/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,16 @@
-function findMissingLetter() {}
+function findMissingLetter(inputArr) {
+    let realAlphabet ="abcdefghijklmnopqrstuvwxyz";
+    let startingChar = inputArr[0];
+    if (startingChar === startingChar.toUpperCase()) {
+        realAlphabet = realAlphabet.toUpperCase();
+    }
+    let startIndex = realAlphabet.indexOf(startingChar);
+    for (let i = startIndex; i < startIndex + inputArr.length; i++) {
+        if (inputArr.indexOf(realAlphabet[i]) === -1) {
+            return realAlphabet[i]
+        }
+    }
+    return '';
+}
 
 module.exports = findMissingLetter;
