464345fb7dbb0fbc5e409dadb681704e65b8a239
mikalikahi <sweeney.phil@gmail.com>
2023-12-17 21:38:46 +0800

    Fixed python test (previously done in Chromebook Debian command line) using Pycharm on Ubuntu/Thinkpad at home

--- Python/AlgorithmPractice/find_missing_number/test_find_missing_number.py
+++ Python/AlgorithmPractice/find_missing_number/test_find_missing_number.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from find_missing_number.find_missing_number import FindMissingNumber
+from find_missing_number import FindMissingNumber
 
 
 class TestFindMissingNumber(TestCase):
@@ -7,7 +7,7 @@ class TestFindMissingNumber(TestCase):
     def setUp(self):
         self.test_obj1 = FindMissingNumber([1, 2, 3, 5, 6, 7, 8])
         self.test_obj2 = FindMissingNumber([])
-        self.test_obj3 = AllCharsAreUnique([2, 1, 5, 3, 8, 7, 6, 10, 9])
+        self.test_obj3 = FindMissingNumber([2, 1, 5, 3, 8, 7, 6, 10, 9])
 
     def test_get_verdict(self):
         self.assertEqual(self.test_obj1.get_missing_number(), 4.0)
