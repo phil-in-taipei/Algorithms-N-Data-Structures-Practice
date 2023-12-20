f1fc18170f6e7c759310f74d9914b9f81b9d4bab
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-20 16:09:40 +0800

    Javascript implementation of valid anagram checker and hashtag generator

--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,33 @@
-function validAnagrams() {}
+
+// my simple approach: (sort both strings into alphabetical order and check if they are the same)
+/*
+function validAnagrams(string1, string2) {
+    return string1.split("").sort().join("") === string2.split("").sort().join("");
+}
+*/
+
+// approach comparing objects with frequency counts
+
+function getFrequencyCount(string) {
+    const frequencyCount = string.split("").reduce((acc, char) => {
+        acc[char] = (acc[char] || 0) + 1;
+        return acc;
+    }, {})
+    return frequencyCount;
+}
+
+function validAnagrams(string1, string2) {
+    const frequencyCountObj1 = getFrequencyCount(string1);
+    const frequencyCountObj2 = getFrequencyCount(string2);
+    return Object.keys(frequencyCountObj1).every((char) =>
+        frequencyCountObj1[char] === frequencyCountObj2[char]
+    ) && Object.keys(frequencyCountObj2).every((char) =>
+        frequencyCountObj2[char] === frequencyCountObj1[char]
+    )
+    // note: last two lines modified from the solution in video to get the test to pass
+    // originally there is an edge case where the 2nd object could contain all of the same
+    // characters and counts as the first, but also have other additional characters
+    // This now checks that the inverse is also true comparing obj2 to obj1
+}
 
 module.exports = validAnagrams;
--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,12 @@
-function generateHashtag() {}
+function generateHashtag(inputString) {
+    const trimmedString = inputString.trim();
+    if(trimmedString.length > 0 && trimmedString.length < 141) {
+        return `#${
+            trimmedString.split(" ").map((word) => {
+                return word.charAt(0).toUpperCase() + word.substring(1)
+        }).join("")}`
+    }
+    return false;
+}
 
 module.exports = generateHashtag;
