c35db1d4fda5d37890535f816588a17ed53fb024
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-21 17:29:24 +0800

    Find missing letter refactored implementation in Javascript (my approach finding differences in unicode values -- more mathematical)

--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,21 @@
-function findMissingLetter() {}
+// My first approach:
+
+function findMissingLetter(arrayOfLetters) {
+    const firstLetterUnicodeValue = arrayOfLetters[0].charCodeAt(0);
+    const lastLetterUnicodeValue = arrayOfLetters[arrayOfLetters.length -1].charCodeAt(0);
+    const expectedSumOfUnicodeValues = (lastLetterUnicodeValue * (lastLetterUnicodeValue + 1) / 2);
+    const sumOfUnicodeValuesUpToFirstLetterInArray = (firstLetterUnicodeValue * (firstLetterUnicodeValue + 1) / 2);
+    const sumOfUnicodeValuesInArray = arrayOfLetters.reduce((total, char) => {
+        return total + char.charCodeAt(0);
+    }, 0);    
+    const differenceInUnicodeValues = expectedSumOfUnicodeValues - (
+                                            sumOfUnicodeValuesInArray + sumOfUnicodeValuesUpToFirstLetterInArray
+                                        );
+    const unicodeValueOfMissingLetter = firstLetterUnicodeValue + differenceInUnicodeValues;
+    return String.fromCharCode(unicodeValueOfMissingLetter);
+}
+
+
+
 
 module.exports = findMissingLetter;
