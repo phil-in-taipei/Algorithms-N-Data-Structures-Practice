ed840dc95f98e918b758c0a6372fad17c1ddab1d
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-21 11:45:18 +0800

    Implementation of car mileage analysis and ipv4 validator in Javascript

--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,16 @@
-const isValidIPv4 = () => {};
+const beginsWithZero = (subAddress) => subAddress.charAt(0) == 0;
+
+const isBetweenZeroAndTwoFiftyFive = (subAddress) => subAddress > 0 && subAddress <= 255;
+
+const isValidIPv4 = (address) => {
+    const addressAsArray = address.split(".");
+    if (addressAsArray.length === 4) {
+        return addressAsArray.every((subAddress) => 
+            !beginsWithZero(subAddress) && isBetweenZeroAndTwoFiftyFive(subAddress)
+        );
+    }
+    return false;
+    
+};
 
 module.exports = isValidIPv4;
--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,34 @@
-function analyzeCarMileage() {}
+
+
+const arrayOfCarMileages = (cars) => cars.map((car) => car.mileage);
+
+function getCarWithHighestMileage(cars) {
+    const carMileages = arrayOfCarMileages(cars);
+    const highestMileage =  Math.max.apply(Math, carMileages);
+    const indexOfCarWithHighestMileage = carMileages.indexOf(highestMileage);
+    return cars[indexOfCarWithHighestMileage];
+}
+
+function getCarWithLowestMileage(cars) {
+    const carMileages = arrayOfCarMileages(cars);
+    const highestMileage =  Math.min.apply(Math, carMileages);
+    const indexOfCarWithHighestMileage = carMileages.indexOf(highestMileage);
+    return cars[indexOfCarWithHighestMileage];
+};
+
+const getTotalMileage = (cars) => cars.reduce((total, car) => {
+    return total + car.mileage;
+}, 0);
+
+
+function analyzeCarMileage(cars) {
+    return {
+           averageMileage: getTotalMileage(cars) / cars.length,
+           highestMileageCar: getCarWithHighestMileage(cars),
+           lowestMileageCar: getCarWithLowestMileage(cars),
+           totalMileage: getTotalMileage(cars)
+        }
+
+}
 
 module.exports = analyzeCarMileage;
