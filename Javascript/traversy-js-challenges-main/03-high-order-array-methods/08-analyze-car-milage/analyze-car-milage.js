106755fcfca2515946d956261789d612a72787c3
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-21 16:26:37 +0800

    Javascript -- refactored car-mileage anaysis and implemented password validator

--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -2,6 +2,7 @@
 
 const arrayOfCarMileages = (cars) => cars.map((car) => car.mileage);
 
+/*
 function getCarWithHighestMileage(cars) {
     const carMileages = arrayOfCarMileages(cars);
     const highestMileage =  Math.max.apply(Math, carMileages);
@@ -15,6 +16,20 @@ function getCarWithLowestMileage(cars) {
     const indexOfCarWithHighestMileage = carMileages.indexOf(highestMileage);
     return cars[indexOfCarWithHighestMileage];
 };
+*/
+
+const getCarWithHighestMileage = (cars) => cars.reduce(
+        (carWithHighestMileage, car) => (
+            carWithHighestMileage.mileage > car.mileage ? carWithHighestMileage : car
+        ), cars[0]
+    );
+
+const getCarWithLowestMileage = (cars) => cars.reduce(
+        (carWithHighestMileage, car) => (
+            carWithHighestMileage.mileage < car.mileage ? carWithHighestMileage : car
+        ), cars[0]
+    );
+
 
 const getTotalMileage = (cars) => cars.reduce((total, car) => {
     return total + car.mileage;
--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/09-password-validator/password-validator.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,21 @@
-function validatePassword() {}
+const itHasAtLeastEightCharacters = (string) => string.length >= 8;
+
+const itHasAtLeastOneUpperCaseLetter = (stringCharsArray) => stringCharsArray.some(
+    (char) => char.charCodeAt(0) > 64 && char.charCodeAt(0) < 91);
+
+const itHasAtLeastOneLowerCaseLetter = (stringCharsArray) => stringCharsArray.some(
+        (char) => char.charCodeAt(0) > 96 && char.charCodeAt(0) < 123);
+
+const itHasAtLeastOneNumericDigit = (stringCharsArray) => stringCharsArray.some(
+        (char) => char.charCodeAt(0) > 47 && char.charCodeAt(0) < 58);
+    
+
+function validatePassword(string) {
+    const stringCharsArray = string.split("");
+    return  itHasAtLeastEightCharacters(string)
+            && itHasAtLeastOneLowerCaseLetter(stringCharsArray) 
+            && itHasAtLeastOneUpperCaseLetter(stringCharsArray) 
+            && itHasAtLeastOneNumericDigit(stringCharsArray);
+};
 
 module.exports = validatePassword;
