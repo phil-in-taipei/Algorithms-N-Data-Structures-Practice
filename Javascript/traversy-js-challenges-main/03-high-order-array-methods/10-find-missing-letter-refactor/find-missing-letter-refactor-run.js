33936ab9b587643d25a3f5dcc4d3b0b7eafdf726
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-22 14:07:51 +0800

    Applied and modified alternate approach to implementing find missing letter challenge in Javascript

--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor-run.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor-run.js
@@ -3,3 +3,6 @@ const findMissingLetter = require('./find-missing-letter-refactor');
 const result = findMissingLetter(['a', 'b', 'c', 'e']);
 
 console.log(result);
+
+const result2 = findMissingLetter(['B', 'C', 'D', 'E', 'F', 'G']);
+console.log(result2)
--- Javascript/traversy-js-challenges-main/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ Javascript/traversy-js-challenges-main/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,5 +1,6 @@
 // My first approach:
 
+/*
 function findMissingLetter(arrayOfLetters) {
     const firstLetterUnicodeValue = arrayOfLetters[0].charCodeAt(0);
     const lastLetterUnicodeValue = arrayOfLetters[arrayOfLetters.length -1].charCodeAt(0);
@@ -7,14 +8,29 @@ function findMissingLetter(arrayOfLetters) {
     const sumOfUnicodeValuesUpToFirstLetterInArray = (firstLetterUnicodeValue * (firstLetterUnicodeValue + 1) / 2);
     const sumOfUnicodeValuesInArray = arrayOfLetters.reduce((total, char) => {
         return total + char.charCodeAt(0);
-    }, 0);    
-    const differenceInUnicodeValues = expectedSumOfUnicodeValues - (
-                                            sumOfUnicodeValuesInArray + sumOfUnicodeValuesUpToFirstLetterInArray
-                                        );
+    }, 0);   
+    const sumOfUnicodeValuesInArrayAndUpToFirstLetterInArray = sumOfUnicodeValuesInArray + sumOfUnicodeValuesUpToFirstLetterInArray
+    const differenceInUnicodeValues = expectedSumOfUnicodeValues - sumOfUnicodeValuesInArrayAndUpToFirstLetterInArray
     const unicodeValueOfMissingLetter = firstLetterUnicodeValue + differenceInUnicodeValues;
-    return String.fromCharCode(unicodeValueOfMissingLetter);
+  return unicodeValueOfMissingLetter ? String.fromCharCode(unicodeValueOfMissingLetter) : '';
 }
+*/
 
+function findMissingLetter(arr) {
+    let previousCode = arr[0].charCodeAt(0); // starts with the code of the first array
+    const missingCharCode = arr.slice(1) // start from 2nd item in array
+      .map((char) => { return char.charCodeAt(0)})
+      .find((current) => {
+        console.log(`This is the difference: ${current - previousCode}`)
+        if (current - previousCode > 1) {
+          return true;
+        }
+        previousCode = current; // next iteration will check whether it is one more than the item at this index
+        return false;
+      });
+    // if `missingCharCode` is truthy then return the character that is one less than the character code
+    return missingCharCode ? String.fromCharCode(missingCharCode - 1) : '';
+  }
 
 
 
