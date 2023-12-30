34b3d1d88ad4cda7521c3e3d123c8283a2b3d8d8
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-12-30 15:00:56 +0800

    Javascript implementation of max sum of subarray values (with debugging of test)

--- Javascript/traversy-js-challenges-main/05-complexity/10-max-subarray-linear/max-subarray-linear-run.js
+++ Javascript/traversy-js-challenges-main/05-complexity/10-max-subarray-linear/max-subarray-linear-run.js
@@ -1,7 +1,12 @@
 const maxSubarraySum = require('./max-subarray-linear');
 
-const arr = [2, 5, 3, 1, 11, 7, 6, 4];
+//const arr = [2, 5, 3, 1, 11, 7, 6, 4];
 
-const result = maxSubarraySum(arr, 3);
+//const result = maxSubarraySum(arr, 3);
+//console.log(result);
 
-console.log(result);
+
+const arr2 = [-2, -5, -3, -1, -11, -7, -6, -4];
+const k2 = 4;
+const result2 = maxSubarraySum(arr2, k2)
+console.log(result2)
