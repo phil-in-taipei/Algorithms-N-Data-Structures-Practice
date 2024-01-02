c7381c103140c8c784ff99e168ec929283bbccab
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-01-02 21:35:48 +0800

    First version of longest consecutive sequence

--- Javascript/traversy-js-challenges-main/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ Javascript/traversy-js-challenges-main/06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,18 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(arrayOfNumbers) {
+    let longestConsecutive = 0;
+    let countingConsecutive = 1;
+    const setOfNumbers = Array.from(new Set(arrayOfNumbers.sort(function (a, b) {  return a - b;  })));
+    for (let i =1; i < setOfNumbers.length; i++) {
+        if (setOfNumbers[i] - setOfNumbers[i  -1] === 1) {
+            countingConsecutive++;
+            if (countingConsecutive > longestConsecutive) {
+                longestConsecutive = countingConsecutive;
+            } 
+        } else {
+            countingConsecutive = 1;
+        }
+    }
+    return longestConsecutive;
+}
 
 module.exports = longestConsecutiveSequence;
