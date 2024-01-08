892de4cdc23aaf254594b0dcfe8158de1dfd5e27
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-01-08 16:36:26 +0800

    Linked list implementation in Javascript  -- added debugging functionality to log logical processes

--- Javascript/traversy-js-challenges-main/07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
+++ Javascript/traversy-js-challenges-main/07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
@@ -11,16 +11,29 @@ class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.debug = false;
     }
 
     add(data) {
+        if (this.debug) {
+            console.log(`Adding method called.......`)
+        }
         const node = new Node(data);
         if (this.head === null) {
+            if (this.debug) {
+                console.log(`There is not  a head, so setting this as head: ${node.data}`)
+            }
             this.head = node;
         } else {
+            if (this.debug) {
+                console.log(`There is a head (${this.head.data}), and the current tail is: ${this.tail.data}`)
+            }
             this.tail.next = node;
         }
         this.tail = node;
+        if (this.debug) {
+            console.log(`New tail is ${this.tail.data}`)
+        }
     }
 
     printAll() {
@@ -32,11 +45,12 @@ class LinkedList {
     }
 
     
-    printAllDebug() {
-        let current = this.head;
-        while(current !== null) {
-            current = current.next;
-        }
+    setDebug() {
+        this.debug = true;
+    }
+
+    turnOffDebug() {
+        this.debug = false;
     }
 }
 
