77ba39f1a92d65cae2dcb996eea656923965f86f
mikalikahi <sweeney.phil@gmail.com>
2024-01-13 10:53:10 +0800

    Modified python implementation of password validator to use any / in list approach

--- Python/ListMethodsPractice/validators/password_validator.py
+++ Python/ListMethodsPractice/validators/password_validator.py
@@ -7,13 +7,16 @@ class PasswordValidator:
         return len(self.string) >= 8
 
     def _has_at_least_one_lowercase_character(self):
-        return len([c for c in self.string if c.islower()]) > 0
+        #return len([c for c in self.string if c.islower()]) > 0
+        return any(ele.islower() for ele in list(self.string))
 
     def _has_at_least_one_uppercase_character(self):
-        return len([c for c in self.string if c.isupper()]) > 0
+        #return len([c for c in self.string if c.isupper()]) > 0
+        return any(ele.isupper() for ele in list(self.string))
 
     def _has_at_least_one_numeric_digit_character(self):
-        return len([c for c in self.string if c.isdigit()]) > 0
+        #return len([c for c in self.string if c.isdigit()]) > 0
+        return any(ele.isdigit() for ele in list(self.string))
 
     def string_is_a_valid_password(self):
         return (
