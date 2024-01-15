185b017b5c859121637f17a938e90e30c93840d4
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-01-15 16:47:58 +0800

    Java implementation of basic recusion countdown with System.out.println() and unit test

---
+++ Java/RecursionPractice/src/calculations/CountDownTest.java
@@ -0,0 +1,21 @@
+package calculations;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CountDownTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void output() {
+    }
+}
