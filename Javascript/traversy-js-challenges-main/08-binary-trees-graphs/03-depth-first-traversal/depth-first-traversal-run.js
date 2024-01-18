7c3ad9ecc7f6faf131f8876895b8ec50dd805dfa
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-01-18 16:14:16 +0800

    Added another layer of depth to the tree to experiment with traversying

--- Javascript/traversy-js-challenges-main/08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal-run.js
+++ Javascript/traversy-js-challenges-main/08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal-run.js
@@ -3,21 +3,47 @@ const { Node, depthFirstTraversal } = require('./depth-first-traversal');
 // Create a binary tree:      a
 //                          /   \
 //                         b     c
-//                        / \    /
-//                       d   e  f
-
+//                        / \    |\
+//                       /   \   | \
+//                      |     |   \  \
+//                      d     e    f   g
+//                     / \   / \   /\  |\
+//                    h   i j  k  l m  n  o
+//
 const a = new Node("a");
 const b = new Node("b");
 const c = new Node("c");
 const d = new Node("d");
 const e = new Node("e");
 const f = new Node("f");
+const g = new Node("g");
+const h = new Node("h");
+const i = new Node("i");
+const j = new Node("j");
+const k = new Node("k");
+const l = new Node("l");
+const m = new Node("m");
+const n = new Node("m");
+const o = new Node("o");
+
+
+
+
 
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
-c.right = f;
+c.right = g;
+c.left = f;
+d.right = i;
+d.left = h;
+e.left = j;
+e.right = k;
+f.left = l;
+f.right = m;
+g.left = n;
+g.right = o;
 console.log('-------------------------------------------------------------------------------')
 console.log('Created nodes and now ititializing depth first traversal object.....')
 console.log('-------------------------------------------------------------------------------')
