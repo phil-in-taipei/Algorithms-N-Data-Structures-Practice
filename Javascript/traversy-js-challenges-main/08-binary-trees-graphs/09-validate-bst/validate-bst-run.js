cf8f01e23eacd40c08c85b699d119bc49c8baca0
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-01-20 11:50:49 +0800

    Notes and console logging for implementation of bst validator in Javascript

--- Javascript/traversy-js-challenges-main/08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
+++ Javascript/traversy-js-challenges-main/08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
@@ -6,4 +6,20 @@ const { Node, isValidBST } = require('./validate-bst');
 //     4   10
 //    / \
 //   2   6
+const root = new Node(8);
+const four = new Node(4);
+const ten = new Node(10);
 
+root.left = four;
+root.right = ten;
+
+const two = new Node(2);
+const six = new Node(6);
+
+four.left = two;
+four.right = six;
+console.log('-------------------------------------------------------------------------------------------')
+const result = isValidBST(root);
+console.log('-------------------------------------------------------------------------------------------')
+console.log(`It is a valid bst: ${result}`);
+console.log('-------------------------------------------------------------------------------------------')
--- Javascript/traversy-js-challenges-main/08-binary-trees-graphs/09-validate-bst/validate-bst.js
+++ Javascript/traversy-js-challenges-main/08-binary-trees-graphs/09-validate-bst/validate-bst.js
@@ -1,3 +1,6 @@
+const Stack = require('../../07-stacks-queues-linked-lists/02-stack-implementation/stack');//require('./stack');
+
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -6,6 +9,36 @@ class Node {
   }
 }
 
-function isValidBST() {}
+function isValidBST(root) {
+  let iterationNumber = 0;
+  function isValidSubtree(node, min, max) {
+    iterationNumber++;
+    console.log('*********************************************************************')
+    console.log(`Recursive iteration number ${iterationNumber} with min: ${min} and max ${max}`)
+    if (!node) {
+      console.log(`and no node -- so finishing subtree -- valid`)
+      return true;
+    }
+    console.log(`And node with this value: ${node.value}`)
+    if (
+      (min !== null && node.value <= min) ||
+      (max !== null && node.value >= max)
+    ) {
+      console.log(`It's false`)
+      console.log(`First condition (value less than or equal to min): ${min !== null && node.value <= min}`)
+      console.log(`Second condition (value greater than or equal to max): ${max !== null && node.value >= max}`)
+      return false;
+    }
+
+    console.log(`Valid: calling again with left and right nodes`)
+    console.log("###################################################################")
+    return (
+      isValidSubtree(node.left, min, node.value) &&
+      isValidSubtree(node.right, node.value, max)
+    );
+  }
+  console.log(`Beginning the process with root value: ${root.value} and no min or max`)
+  return isValidSubtree(root, null, null);
+}
 
 module.exports = { Node, isValidBST };
