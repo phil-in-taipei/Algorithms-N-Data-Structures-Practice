cd7c52bd94214924282205e0ce3e6cd10c08b8e1
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-01-22 17:08:19 +0800

    Bubble sort implementation in Javascript

--- Javascript/traversy-js-challenges-main/09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ Javascript/traversy-js-challenges-main/09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,18 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+    let swappingInProcess = true;
+    while(swappingInProcess) {
+        swappingInProcess = false; 
+        for (let i = 1; i < array.length + 1; i++){
+            if (array[i] < array[i - 1]) {
+                let temp = array[i - 1];
+                array[i - 1] = array[i];
+                array[i] = temp;
+                swappingInProcess = true;
+            }
+        }
+    }
+    return array;
+
+}
 
 module.exports = bubbleSort;
