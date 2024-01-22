0f6bca722e706a6c81e13404e4059c704add3ac0
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-01-22 14:50:54 +0800

    Alternative approach to graph-depth-first traversal in Javascript using a map with already visited vertices as keys and true as values and returning the keys as an array

--- Javascript/traversy-js-challenges-main/08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ Javascript/traversy-js-challenges-main/08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,7 @@
 const Stack = require('./stack');
 
+// approach with just an array
+/*
 function depthFirstTraversal(graph, startingVertex) {
     if (!graph.adjacencyList[startingVertex]) {
         return [];
@@ -24,6 +26,35 @@ function depthFirstTraversal(graph, startingVertex) {
     }
 
     return result;
+
+}
+*/
+
+// 2nd approach with Map of results
+function depthFirstTraversal(graph, startingVertex) {
+    if (!graph.adjacencyList[startingVertex]) {
+        return [];
+    }
+
+    const stack = new Stack();
+    const alreadyVistedVertices = new Map();
+
+    stack.push(startingVertex);
+
+
+    while (!stack.isEmpty()) {
+        const currentVertex = stack.pop();
+        if (!alreadyVistedVertices.has(currentVertex)) {
+            alreadyVistedVertices.set(currentVertex, true);
+        }
+        graph.adjacencyList[currentVertex].forEach((neighbor) => {
+            if (!alreadyVistedVertices.has(neighbor)) {
+                stack.push(neighbor);
+            }
+        });
+    }
+
+    return Array.from(alreadyVistedVertices.keys());
 
 }
 
